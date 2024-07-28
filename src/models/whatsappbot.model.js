eb3f9688f81a76666caf17e31eed3ec7094ed519
Heydar Shukurov <heydar.shukurov@gmail.com>
2024-07-28 12:34:50 +0400

    Puppeteer configured to work on server env hopefully ) take 4

--- src/models/whatsappbot.model.js
+++ src/models/whatsappbot.model.js
@@ -1,4 +1,5 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
+const puppeteer = require('puppeteer-core');
 const qrcodeTerminal = require('qrcode-terminal');
 const qrcode = require('qrcode');
 const AI = require('./ai.model');
